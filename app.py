f7b32f590118a78ae657b228d45172ed9b69e755
Niriksha <nirikshakamath7@gmail.com>
2025-10-24 17:42:12 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, jsonify
+import os
+import time
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Hello from Flask API!"})
+APP_VERSION = os.getenv("APP_VERSION", "v1")
+APP_MESSAGE = os.getenv("APP_MESSAGE", "Hello from Flask in a container!")
 
-if __name__ == '__main__':
-    app.run(host ='0.0.0.0',port=5000)
+@app.route("/")
+def home():
+    return jsonify({
+        "message": APP_MESSAGE,
+        "version": APP_VERSION,
+        "time": time.time()
+    })
+
+@app.route("/healthz")
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
